efffaa51f13b42e3072d377703ef0b6f74bfa51f
bimManager <bim.kpln@gmail.com>
2022-11-24 17:06:42 +0300

    MEP. Пакетный флиппер элементов

---
+++ pyKPLN_MEP/KPLN.extension/pyKPLN_MEP.tab/ЭОМ_СС.panel/flipper.pushbutton/AppForm.py
@@ -0,0 +1,110 @@
+#encoding: utf-8 python
+import clr
+clr.AddReference("System.Windows.Forms")
+clr.AddReference("System.Drawing")
+from System.Windows.Forms import *
+from System.Drawing import Size, Point, Image, ContentAlignment, Font, FontStyle, Color, SystemColors
+from pyrevit import script
+from System import EventHandler
+import webbrowser as wb
+
+class KPLN_Alarm_Form(Form):
+    def __init__(self, label_text):
+        self.label_text = label_text
+        self.script_path = script.get_script_path()
+
+        self.IMAGE_LOGO = image_logo = Image.FromFile("".join([self.script_path, "\\", r"Images\Background.png"]))
+        self.IMAGE_OK = Image.FromFile("".join([self.script_path, "\\", r"Images\button_OK.png"]))
+        self.IMAGE_OK_WHITE = Image.FromFile("".join([self.script_path, "\\", r"Images\button_OK_White.png"]))
+        self.IMAGE_Q = Image.FromFile("".join([self.script_path, "\\", r"Images\button_Q.png"]))
+        self.IMAGE_Q_WHITE = Image.FromFile("".join([self.script_path, "\\", r"Images\button_Q_White.png"]))
+
+        self.COLOR = SystemColors.GradientInactiveCaption
+        self.Opacity = 0.95
+
+        self.InitComponent()
+
+    def InitComponent(self):
+        #
+        #   ОСНОВНАЯ ФОРМА
+        #
+        self.StartPosition = FormStartPosition.CenterScreen
+        self.Size = Size(500, 300)
+        self.FormBorderStyle = FormBorderStyle.None
+        image_logo = self.IMAGE_LOGO
+        self.BackgroundImage = image_logo
+        self.BackColor = self.COLOR
+        #
+        #   КНОПКА ОК
+        #
+        self.button_OK = Button()
+        self.button_OK.Location = Point(150, 215)
+        self.button_OK.Size = Size(125, 56)
+        self.button_OK.FlatAppearance.BorderSize = 0
+        self.button_OK.FlatStyle = FlatStyle.Flat
+        self.button_OK.BackgroundImage = self.IMAGE_OK
+        self.button_OK.Cursor = Cursors.Hand
+        self.button_OK.Click += self.click_OK
+        self.button_OK.MouseHover += EventHandler(self.change_OK_hover)
+        self.button_OK.MouseLeave += EventHandler(self.change_OK_leave)
+        #
+        #   КНОПКА ?
+        #
+        self.button_Q = Button()
+        self.button_Q.Location = Point(295, 215)
+        self.button_Q.Size = Size(56, 56)
+        self.button_Q.FlatAppearance.BorderSize = 0
+        self.button_Q.FlatStyle = FlatStyle.Flat
+        self.button_Q.BackgroundImage = self.IMAGE_Q
+        self.button_Q.Cursor = Cursors.Hand
+        self.button_Q.Click += self.click_Q
+        self.button_Q.MouseHover += EventHandler(self.change_Q_hover)
+        self.button_Q.MouseLeave += EventHandler(self.change_Q_leave)
+        #
+        #   ЯРЛЫК ТЕКСТА
+        #
+        self.label = Label()
+        self.label.Location = Point(36, 37)
+        self.label.Size = Size(427, 164)
+        self.label.Text = self.label_text
+        self.label.TextAlign = ContentAlignment.MiddleCenter
+        self.label.Font = Font("Century Gothic", 16, FontStyle.Regular)
+        self.hidden_button = Button()
+        self.hidden_button.Location = Point(490,0)
+        self.hidden_button.Size = Size(10,10)
+        self.hidden_button.BackColor = Color.Black
+        self.hidden_button.FlatAppearance.BorderSize = 0
+        self.hidden_button.FlatStyle = FlatStyle.Flat
+        self.hidden_button.Click += self.go
+        #
+        #controls
+        #
+        self.Controls.Add(self.button_OK)
+        self.Controls.Add(self.button_Q)
+        self.Controls.Add(self.label)
+        self.Controls.Add(self.hidden_button)
+
+    def click_OK(self, sender, args):
+        self.Close()
+
+    def click_Q(self, sender, args):
+        wb.open_new("http://moodle.stinproject.local/mod/book/view.php?id=502&chapterid=1100")
+
+    def change_OK_hover(self, sender, args):
+        self.button_OK.BackgroundImage = self.IMAGE_OK_WHITE
+
+    def change_OK_leave(self, sender, args):
+        self.button_OK.BackgroundImage = self.IMAGE_OK
+
+    def change_Q_hover(self, sender, args):
+        self.button_Q.BackgroundImage = self.IMAGE_Q_WHITE
+
+    def change_Q_leave(self, sender, args):
+        self.button_Q.BackgroundImage = self.IMAGE_Q
+
+    def go(self, sender, args):
+        wb.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+class KPLN_Alarm:
+    def __init__(self, text):
+        Application.Run(KPLN_Alarm_Form(text))
---
+++ pyKPLN_MEP/KPLN.extension/pyKPLN_MEP.tab/ЭОМ_СС.panel/flipper.pushbutton/script.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+__title__ = 'Флиппер'
+__doc__ = '''Данный скрипт переворачивает осветительные приборы относительно опорной плоскости, на которой они расположены.
+Объекты могут быть выбраны заранее, либо после запуска скрипта. Выбор не обязательно отфильтровывать. Перевернуться только осветительные приборы.
+
+Прим.: Подробное описание смотри на MOODLE'''
+__author__ = "@butiryc_acid" #TELEGRAM
+
+from Autodesk.Revit import DB, UI
+from pyrevit import script
+
+from AppForm import KPLN_Alarm
+
+#region ФУНКЦИИ, КЛАССЫ, КОНСТАНТЫ:
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+
+CATEGORY = "Осветительные приборы"
+
+class _ISelectionFilter(UI.Selection.ISelectionFilter):
+    '''Интерфейс RevitAPI
+    Используется для фильтрации по категории
+
+    '''
+    def AllowElement(self, element):
+        if element.Category.Name == CATEGORY:
+            return True
+        return False
+#endregion
+
+#region ВЫБОРКА ЭЛЕМЕНТОВ
+element_ids = list(uidoc.Selection.GetElementIds()) # Проверка на "выбранность"
+if len(element_ids) != 0:
+    lighting_fixtures = [doc.GetElement(i) for i in element_ids if doc.GetElement(i).Category.Name == CATEGORY]
+    if len(lighting_fixtures) == 0:
+        KPLN_Alarm("В выбранных элементах нет "+CATEGORY)
+        script.exit()
+elif len(element_ids) == 0:
+    filter_ = _ISelectionFilter()
+    object_type = UI.Selection.ObjectType.Element
+    try:
+        reference_lighting_fixtures = uidoc.Selection.PickObjects(object_type, filter_, "Выберите "+CATEGORY)
+    except:
+        KPLN_Alarm("Выбор элементов отменен")
+        script.exit()
+    if len(reference_lighting_fixtures) == 0:
+        KPLN_Alarm("Не выбран ни один экземпляр")
+        script.exit()
+    lighting_fixtures = [doc.GetElement(i) for i in reference_lighting_fixtures]
+#endregion
+
+#region ПРОВЕРКА НА ПЕРЕВОРОТ
+if not all([element.CanFlipWorkPlane for element in lighting_fixtures]):
+    KPLN_Alarm(("".join(["Не все ", CATEGORY, " могут быть перевернуты"])))
+    script.exit()
+#endregion
+
+#region ОСНОВНОЙ ЦИКЛ. ПЕРЕВОРОТ ОБЪЕКТОВ
+transaction = DB.Transaction(doc, "".join(["pyKPLN_", __title__]))
+transaction.Start()
+
+try:
+    for element in lighting_fixtures:
+        element.IsWorkPlaneFlipped = not(element.IsWorkPlaneFlipped)
+except:
+    KPLN_Alarm("Произошла непредвиденная ошибка\nОбратитесь в BIM-Отдел")
+    transaction.RollBack()
+    script.exit()
+
+transaction.Commit()
+KPLN_Alarm("Элементы успешно повернуты")
+#endregion
